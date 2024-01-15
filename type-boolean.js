883449813007aea176fcae0c8437f55b235b0598
Nicolas Azevedo <148501878+Nicolinhas08@users.noreply.github.com>
2024-01-15 15:21:41 -0300

    Add files via upload

---
+++ type-boolean.js
@@ -0,0 +1,14 @@
+// boolean
+// true -> verdadeiro
+// false -> falso
+
+const primeiroNumero = 10;
+const segundoNumero = 10;
+let cadastroAtivo = true;
+
+// console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2)
